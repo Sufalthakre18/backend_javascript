0e780f1c62bdb0f158a5c106c8bf54c983c0eb02
Sufalthakre18 <sufalthakre1@gmail.com>
2025-04-19 09:59:01 +0530

    feat: add dashboard routes and controller for channel statistics and videos

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 
 // routes declaration
@@ -41,6 +42,7 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 
 
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose"
+import { Video } from "../models/video.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import { Like } from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    // Get stats for the logged-in user's channel
+    const userId = req.user._id;
+
+    const totalSubs = await Subscription.find({ channel: userId })
+
+    if (!totalSubs) {
+        return new ApiResponse(201, "Your Channel haven't anu Subs")
+    }
+
+    const totalVideos = await Video.find({ owner: userId })
+
+    if (!totalVideos) {
+        return new ApiResponse(201, "Your Channel haven't uploaded any videos")
+    }
+
+    const videos = await Video.find({ owner: userId }, "_id")
+    const videoIds = videos.map(video => video._id)
+    const totalLikes = await Like.countDocuments({
+        video: { $in: videoIds },
+        liked: true // Assuming a "liked" boolean field exists
+    });
+
+    return res.status(200).json(
+        new ApiResponse(200,"Your dashboard fetched successfullly",{
+            totalVideos,
+            totalSubs,
+            totalLikes,
+            totalsubscriber:totalSubs.length,
+            totalvideos:totalVideos.length
+        })
+    )
+        
+})
+
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+    const userId = req.user._id;
+    const videos = await Video.find({ owner: userId })
+
+    if (!videos) {
+        return new ApiResponse(201, "Your Channel haven't uploaded any videos")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, "all videos fetched successfully for channel", { videos, totalVideos: videos.length })
+    )
+})
+
+export {
+    getChannelStats,
+    getChannelVideos
+}
